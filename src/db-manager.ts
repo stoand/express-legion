d3e5d9b920ec54443bb79e2e96c7fedc379d4701
Andreas Stocker <stoand@protonmail.com>
2025-06-04 22:11:43 +0200

    db-manager: SetupPostgres

---
+++ src/db-manager.ts
@@ -0,0 +1,35 @@
+import { join } from 'path';
+import { exists, mkdir } from 'fs/promises';
+import { execSync } from 'child_process';
+
+const DEFAULT_LEGION_RUNTIME_DIR = 'legion_runtime';
+
+const DEFAULT_POSTGRES_DIR = '/lib/postgresql/16/bin/';
+
+const baseConfig = {
+  runtimeDir: DEFAULT_LEGION_RUNTIME_DIR,
+  postgresBinPath: DEFAULT_POSTGRES_DIR,
+};
+
+export type SetupPostgresConfig = Partial<typeof baseConfig>;
+
+export async function setupPostgres(partialConfig: SetupPostgresConfig) {
+
+  const config = Object.assign(baseConfig, partialConfig);
+
+  if (!await exists(join(config.postgresBinPath, 'postgres'))) {
+    throw new Error(`Invalid postgres bin directory "${config.postgresBinPath}"`);
+  }
+
+  const fullRuntimePath = join(process.env.PWD || '', 'legion_runtime');
+  const fullPrefabPath = join(fullRuntimePath, 'db_prefab');
+
+  if (!await exists(fullPrefabPath)) {
+    await mkdir(fullRuntimePath, { recursive: true });
+    execSync(`${config.postgresBinPath}/initdb -D ${fullPrefabPath}`);
+  }
+
+  console.log('done');
+}
+
+setupPostgres({});
