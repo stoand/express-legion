34d601c0a016874c066cedc187d85e8f13994131
Andreas Stocker <stoand@protonmail.com>
2025-06-05 19:15:44 +0200

    runner - add  watching ( in progress )

--- src/runner.ts
+++ src/runner.ts
@@ -1,6 +1,8 @@
 import { Worker } from 'worker_threads';
 import { type SetupPostgresConfig, baseConfig as basePostgresConfig, dbSetup, dbTeardown } from './db-manager';
 import { getTestPaths } from './test-manager';
+import { watch } from 'fs';
+import { join } from 'path';
 
 export type { SetupPostgresConfig } from './db-manager';
 export { getTestPaths } from './test-manager';
@@ -9,17 +11,37 @@ const DEFAULT_TEST_DIR = 'integration';
 const DEFAULT_BUILD_DIR = 'legion_out';
 
 const baseConfig = {
+  watchDir: null as string | null,
   testDir: DEFAULT_TEST_DIR,
   buildDir: DEFAULT_BUILD_DIR,
 };
 
+let initedWatcher = false;
+let runAgainAsSoonAsPossible = false;
+
 export type TestConfig = Partial<typeof baseConfig>;
 
+export function initWatcher(watchDir: string) {
+  const fullWatchDir = join(process.cwd(), watchDir);
+  console.log('watching dir', fullWatchDir);
+    watch(fullWatchDir, (_event, filename) => {
+      if (filename && filename.endsWith('.ts')) {
+        runAgainAsSoonAsPossible = true;
+        console.log('run asap')
+      }
+    });
+}
+
 export async function runTests(partialTestConfig: TestConfig, partialPostgresConfig: SetupPostgresConfig) {
+
   const startTime = Date.now();
 
   const testConfig = Object.assign({}, baseConfig, partialTestConfig);
   const postgresConfig = Object.assign({}, basePostgresConfig, partialPostgresConfig);
+  
+  if (!initedWatcher && testConfig && testConfig.watchDir) {
+    initWatcher(testConfig.watchDir);
+  }
 
   const files = await getTestPaths(testConfig.testDir);
 
@@ -57,7 +79,11 @@ export async function runTests(partialTestConfig: TestConfig, partialPostgresCon
           const endTime = Date.now();
           console.log(`Tests done: ${files.length - failedTests} of ${files.length} succeeded in ${endTime - startTime}ms`);
           await dbTeardown(dbProcesses);
-          process.exit(failedTests > 0 ? 1 : 0);
+          if (testConfig.watchDir) {
+            
+          } else {
+            process.exit(failedTests > 0 ? 1 : 0);
+          }
         }
       });
     }
